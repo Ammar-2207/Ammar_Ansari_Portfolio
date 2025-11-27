7c0ff7019a6a5c5c6a6bdd24cb5bbd853153e145
Ammar Ansari <142200656+Ammar-2207@users.noreply.github.com>
2025-11-27 15:32:38 +0530

    Add files via upload

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,89 @@
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const AppShowcase = () => {
+  const sectionRef = useRef(null);
+  const rydeRef = useRef(null);
+  const libraryRef = useRef(null);
+  const ycDirectoryRef = useRef(null);
+
+  useGSAP(() => {
+    // Animation for the main section
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1.5 }
+    );
+
+    // Animations for each app showcase
+    const cards = [rydeRef.current, libraryRef.current, ycDirectoryRef.current];
+
+    cards.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          y: 50,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom-=100",
+          },
+        }
+      );
+    });
+  }, []);
+
+  return (
+    <div id="work" ref={sectionRef} className="app-showcase">
+      <div className="w-full">
+        <div className="showcaselayout">
+          <div ref={rydeRef} className="first-project-wrapper">
+            <div className="image-wrapper">
+              <img src="/images/project1_1.jpg" alt="Student Certificate Verification System" />
+            </div>
+            <div className="text-content">
+              <h2>
+                Streamlining Certificate Verification with a Modern, User-Friendly Web System called Certify.
+              </h2>
+              <p className="text-white-50 md:text-xl">
+                An app built with React, Node.js, Express.js & MongoDB for a fast,
+                user-friendly experience.
+              </p>
+            </div>
+          </div>
+
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="project" ref={libraryRef}>
+              <div className="image-wrapper bg-[#FFEFDB]">
+                <img
+                  src="/images/project2_1.png"
+                  alt="Online Complaint Registration and Management System"
+                />
+              </div>
+              <h2>Online Complaint Registration and Management System</h2>
+            </div>
+
+            <div className="project" ref={ycDirectoryRef}>
+              <div className="image-wrapper bg-[#FFE7EB]">
+                <img src="/images/project3_1.png" alt="Food Delivery App" />
+              </div>
+              <h2>Food Delivery App</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppShowcase;
